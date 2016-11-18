a64fab64c4ea7c1a733e70c40a8cbf520e97f1c4
bangms92 <bangms92@gmail.com>
2016-11-17 22:27:57 -0500

    server updated

--- FTA-server.py
+++ FTA-server.py
@@ -1,33 +1,55 @@
+DEBUG = True
+
+def log(message):
+    if DEBUG:
+        print message
+
 import sys
 import struct
 import socket
 import crpSocket
 
+# FTA-server X
+# X: The port number at which the FTA-server's UDP socket should bind
 def checkArgs():
     if len(sys.argv) != 2:
         print "Invalid arguments"
         sys.exit()
         
-        
-def listen():
+# Main  
+def runServer():
     global sock
+    global state
     
-    sock.listen()
-    
+    if not (state == "LISTENING" or state == "CONNECTED"):
+    	try:
+    		try:
+    			sock.listen()
+    		except Exception as e:
+    			log("Exceptino: " + str(e))
+				sys.exit(0)
+			log("Setting state to connected.\n")
+			state = "CONNECTED"
+		except Exception as e:
+			log("Connection Failed: " + str(e))
+			return
+
+
+
+# ---------------------------------------------------------
 checkArgs()
 
-# FTA-server X
-# X: The port number at which the FTA-server's UDP socket should bind
+serverCRPport = int(sys.argv[1])
 
-#Create Client socket
 sock = crpSocket.CRPSocket(int(sys.argv[1]))
 state = 'DISCONNECTED'
 
 try:
-    sock.bind("127.0.0.1", int(sys.argv[1]))
+    sock.bind("127.0.0.1", serverCRPport)
 except Exception as e:
-    print "Error during binding" + str(e)
+    print "Error: could not bind to port " + str(serverCRPport) + " on local host.\n"
+    log("Exception: " + str(e))
     sys.exit(1)
     
-
-listen()
+while True:
+	runServer()
